e1e2ba111193489bedbd6952a5d014b74f6a09c2
lijin <coolagain2006@gmail.com>
2018-08-02 14:28:28 +0800

    Add bootstrap of jobs

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jobs.apps.JobsConfig',
+    'blog.apps.BlogConfig',
+    # 'jobs.apps.JobsConfig',
+    'jobs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
